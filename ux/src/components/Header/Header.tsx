b239e488151f6700fd789235cdaf8ed62db4d1b3
Steve Michelotti <steve.michelotti@gmail.com>
2021-01-17 20:44:54 -0500

    fixed nav (#19)
    Co-authored-by: Steve Michelotti <steve.michelotti@microsoft.com>

--- ux/src/components/Header/Header.tsx
+++ ux/src/components/Header/Header.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Nav from 'react-bootstrap/esm/Nav';
 import Navbar from 'react-bootstrap/esm/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export const Header: React.FunctionComponent = () => {
+    const links = [
+      { title: 'Home', url: '/' },
+      { title: 'About', url: '/about' },
+      { title: 'How It Works', url: '/process' },
+      { title: 'Contact', url: '/contact' }
+    ];
+
     return (
         <>
           <div className="site-branding-area">
@@ -12,19 +20,18 @@ export const Header: React.FunctionComponent = () => {
           </div>
           <div className="mainmenu-area">
             <div className="container">
-
               <Navbar expand="lg">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav activeKey={window.location.pathname} className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/about">About</Nav.Link>
-                    <Nav.Link href="/process">How It Works</Nav.Link>
-                    <Nav.Link href="/contact">Contact</Nav.Link>
+                  <Nav className="mr-auto">
+                    {links.map(l => (
+                      <LinkContainer to={l.url} exact={true}>
+                        <Nav.Link>{l.title}</Nav.Link>
+                      </LinkContainer>
+                    ))}
                   </Nav>
                 </Navbar.Collapse>
               </Navbar>
-
             </div>
           </div>
         </>
--- ux/src/pages/Admin/components/AdminHeader/AdminHeader.tsx
+++ ux/src/pages/Admin/components/AdminHeader/AdminHeader.tsx
@@ -1,31 +1,40 @@
 import React from 'react';
 import Nav from 'react-bootstrap/esm/Nav';
 import Navbar from 'react-bootstrap/esm/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export const AdminHeader: React.FunctionComponent = () => {
-    return (
-        <>
-          <div className="site-branding-area">
-              <div className="logo">
-                  <h1><img src="/jenna-logo.jpg" alt="logo" /><span className="text-nowrap">Fresh Fit Meals Admin</span></h1>
-              </div>
-          </div>
-          <div className="mainmenu-area">
-              <div className="container">
+  const links = [
+    { title: 'Orders', url: '/admin/orders' },
+    { title: 'Menus', url: '/admin/menus' },
+    { title: 'Menu Items', url: '/admin/menu-items' }
+  ];
 
-                  <Navbar expand="lg">
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                      <Nav activeKey={window.location.pathname} className="mr-auto">
-                        <Nav.Link href="/admin/orders">Orders</Nav.Link>
-                        <Nav.Link href="/admin/menus">Menus</Nav.Link>
-                        <Nav.Link href="/admin/menu-items">Menu Items</Nav.Link>
-                      </Nav>
-                    </Navbar.Collapse>
-                  </Navbar>
+  return (
+      <>
+        <div className="site-branding-area">
+            <div className="logo">
+                <h1><img src="/jenna-logo.jpg" alt="logo" /><span className="text-nowrap">Fresh Fit Meals Admin</span></h1>
+            </div>
+        </div>
+        <div className="mainmenu-area">
+            <div className="container">
 
-              </div>
-          </div>
-        </>
-    );
+                <Navbar expand="lg">
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav activeKey={window.location.pathname} className="mr-auto">
+                    {links.map(l => (
+                      <LinkContainer to={l.url} exact={true}>
+                        <Nav.Link>{l.title}</Nav.Link>
+                      </LinkContainer>
+                    ))}
+                    </Nav>
+                  </Navbar.Collapse>
+                </Navbar>
+
+            </div>
+        </div>
+      </>
+  );
 }
