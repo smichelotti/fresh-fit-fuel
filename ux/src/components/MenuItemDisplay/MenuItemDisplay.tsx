c69fde3eeb27fa529c3e94acfbb0f02291bc5059
Justin Michelotti <jmichelotti@mosaiclearning.com>
2021-01-23 12:04:48 -0500

    getting quantity dropdown to work and display subtotal - still need to get amazon style 10+

--- ux/src/components/MenuItemDisplay/MenuItemDisplay.tsx
+++ ux/src/components/MenuItemDisplay/MenuItemDisplay.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Form } from 'react-bootstrap';
 import { MenuItem } from '../../models/MenuItem';
 
@@ -10,6 +10,11 @@ interface MenuItemDisplayProps {
 
 export const MenuItemDisplay: React.FunctionComponent<MenuItemDisplayProps> = (props) => {
     var n = props.item.price.toFixed(2);
+    const [currQuan, setCurrQuan] = useState(0);
+
+    const onQuantityChange = (event: ChangeEvent<HTMLSelectElement>) => setCurrQuan(parseInt(event.target.value));
+
+    var n2 = (parseInt(n) * currQuan).toFixed(2);
 
     return (
         <>
@@ -28,15 +33,21 @@ export const MenuItemDisplay: React.FunctionComponent<MenuItemDisplayProps> = (p
                         </div>
                         <div className="col-md-3" style={{ margin: '1%' }}>
                             <label style={{ color: 'white', float: 'left' }} htmlFor="quantity">Quantity:</label>
-                            <Form.Control as="select" id="quantity" size="sm">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
+                            <Form.Control as="select" id="quantity" size="sm" onChange={onQuantityChange}>
+                                <option value="0">0</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
                             </Form.Control>
                             <label style={{ color: 'white' }} htmlFor="subtotal">Subtotal:</label>
-                            <p id="subtotal" style={{ color: 'white', float: 'right' }}>$test</p>
+                            <p id="subtotal" style={{ color: 'white', float: 'right' }}>${n2}</p>
 
 
                             <table className="table table-sm macro-info table-bordered">
