1efeb2cc22f1c528a14bb39c07f4bff6f035aa35
Steve Michelotti <steve.michelotti@gmail.com>
2021-01-25 22:55:26 -0500

    finishes to static content (#35)
    * finishes to static content
    
    * tweaked working
    
    Co-authored-by: Steve Michelotti <steve.michelotti@microsoft.com>

--- ux/src/pages/Contact/Contact.tsx
+++ ux/src/pages/Contact/Contact.tsx
@@ -10,38 +10,30 @@ export const Contact: React.FunctionComponent = () => {
 
       <div id="contact-content" className="container" style={{ paddingTop: '25px' }}>
         <div className="row">
-          <div className="col-sm-3 col-sm-offset-3" style={{ display: 'flex' }}>
+          <div className="col-md-6" style={{ display: 'flex' }}>
             <div style={{ margin: 'auto' }}>
               <h2>Social Media</h2>
               <ul className="list-unstyled">
                 <li className="contact-link">
-                  <a href="https://www.instagram.com/jennamichelotti_cf/">
-                    <i className="fa fa-instagram"></i> Instagram
-                            </a>
+                  <a href="https://www.instagram.com/fresh.fit.fuel/">
+                    <i className="fa fa-instagram"></i> fresh.fit.fuel
+                  </a>
                 </li>
               </ul>
             </div>
           </div>
-          <div className="col-sm-3" style={{ display: 'flex' }}>
+          <div className="col-md-6" style={{ display: 'flex' }}>
             <div style={{ margin: 'auto' }}>
               <h2>Contact Info</h2>
               <ul className="list-unstyled who">
-                <li className="contact-link"><a href="mailto:michelotti12@gmail.com"><i className="fa fa-envelope"></i> Email(ToDo)</a></li>
+                <li className="contact-link">
+                  <a href="mailto:freshfitfuelbyjenna@gmail.com">
+                    <i className="fa fa-envelope"></i> freshfitfuelbyjenna@gmail.com
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
-        </div>
-        <div style={{ display: 'flex' }}>
-          <p className="customer-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero
-          quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi
-          iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi
-          veritatis magni at? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero
-          quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi
-          iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi
-          veritatis magni at? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero
-          quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi
-          iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi
-                veritatis magni at?</p>
         </div>
       </div>
     </>
